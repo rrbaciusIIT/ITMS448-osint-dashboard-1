dac6c6624f2aa7032ebd1c480888517f3908fd10
Henry Post <HenryFBP@gmail.com>
2019-11-11 17:24:36 -0600

    fix class not dev

--- bowser/contentFlagger.py
+++ bowser/contentFlagger.py
@@ -1,6 +1,35 @@
 import re
 from typing import List
 
+
+class ContentFlagger:
+	"""A class that can flag content as containing specific words or phrases."""
+
+	def __init__(self, keywords: List[str], regex_matches: List[str]):
+		self.match_words = keywords
+		self.regex_matches = regex_matches
+
+	def flag_content(self, content: str):
+
+		if content is None:  # thanks for the null values json <3 i feel like i'm in Java all over again
+			return False
+
+		# Fix any issues dealing with capitalization
+		content = content.lower
+
+		# check all our keywords
+		for word in self.match_words:
+			if word in content:
+				return True
+
+		# check all our regex
+		for rexp in self.regex_matches:
+			if re.compile(rexp).match(content):
+				return True
+
+		return False
+
+
 ContentFlaggerBadWords = ContentFlagger(
 	keywords=[
 		'nigger', 'negro', 'niglet', 'shitskin',
@@ -39,31 +68,3 @@ ContentFlaggerTerrorist = ContentFlagger(
 		'assassin(|ate(|d))'
 	],
 )
-
-
-class ContentFlagger:
-	"""A class that can flag content as containing specific words or phrases."""
-
-	def __init__(self, keywords: List[str], regex_matches: List[str]):
-		self.match_words = keywords
-		self.regex_matches = regex_matches
-
-	def flag_content(self, content: str):
-
-		if content is None:  # thanks for the null values json <3 i feel like i'm in Java all over again
-			return False
-
-		# Fix any issues dealing with capitalization
-		content = content.lower
-
-		# check all our keywords
-		for word in self.match_words:
-			if word in content:
-				return True
-
-		# check all our regex
-		for rexp in self.regex_matches:
-			if re.compile(rexp).match(content):
-				return True
-
-		return False
