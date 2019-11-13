ceaa7ff6c58343e17efe69f9fa8505862c52bb22
Henry Post <HenryFBP@gmail.com>
2019-11-12 23:09:37 -0600

    fail if FAILED in output

--- ci-scripts/run_bowser_tests.sh
+++ ci-scripts/run_bowser_tests.sh
@@ -2,7 +2,13 @@
 
 pushd bowser
 
-pipenv run coverage run --source=. -m unittest discover
+output=$(pipenv run coverage run --source=. -m unittest discover 2>&1)
+
+
+if grep -q FAILED "${output}"; then
+    echo "FAILED in output!"
+    exit 1
+fi
 
 ls -la .coverage
 
