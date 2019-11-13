b854a99a7e0913fde063e4a4f969fe14037e5ee3
Henry Post <HenryFBP@gmail.com>
2019-11-12 23:13:07 -0600

    no, output var is NOT a file

--- ci-scripts/run_bowser_tests.sh
+++ ci-scripts/run_bowser_tests.sh
@@ -4,8 +4,9 @@ pushd bowser
 
 output=$(pipenv run coverage run --source=. -m unittest discover 2>&1)
 
+echo $output
 
-if grep -q FAILED "${output}"; then
+if echo "${output}" | grep -q FAILED; then
     echo "FAILED in output!"
     exit 1
 fi
