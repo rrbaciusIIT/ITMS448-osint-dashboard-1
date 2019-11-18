4425fcec98dbec8db0b73b66479b746a6ba91526
Henry Post <HenryFBP@gmail.com>
2019-11-17 22:40:24 -0600

    move part of gui to different class

--- bowser/bowserGUI.py
+++ bowser/bowserGUI.py
@@ -1,9 +1,34 @@
-from tkinter import Tk, Label, Button, Listbox, END
+from tkinter import Tk, Label, Button, Listbox, END, EXTENDED, Frame, SUNKEN
 
 from bowserScraper import BOARDS_4PLEBS
 
 
-class MyFirstGUI:
+def greet():
+	print("Greetings!")
+
+
+def nothing():
+	print("I do nothing!")
+
+
+class BowserOptionsPane:
+	def __init__(self, parent):
+
+		self.parent = parent
+
+		self.frame_options = Frame(self.parent, width=200, height=200, relief=SUNKEN)
+		self.frame_options.pack()
+
+		self.listbox_boards = Listbox(self.frame_options, selectmode=EXTENDED)
+		'''List of boards the user wants to save to a CSV file'''
+		self.listbox_boards.pack()
+
+		# add all boards
+		for item in BOARDS_4PLEBS:
+			self.listbox_boards.insert(END, item)
+
+
+class BowserMainGUI:
 	def __init__(self, master):
 		self.master = master
 		master.title("A simple GUI")
@@ -11,28 +36,21 @@ class MyFirstGUI:
 		self.label_hello_world = Label(master, text="This is our first GUI!")
 		self.label_hello_world.pack()
 
-		self.button_greet = Button(master, text="Greet", command=self.greet)
+		self.button_greet = Button(master, text="Greet", command=greet)
 		self.button_greet.pack()
 
-		self.button_generate = Button(master, text="Generate CSV", command=lambda: print("I do nothing!"))
+		self.BowserOptionsPane = BowserOptionsPane(master)
+
+		self.button_generate = Button(master, text="Generate CSV", command=nothing)
 		'''When clicked, generate a CSV file.'''
 		self.button_generate.pack()
-
-		self.listbox_boards = Listbox(master)
-		self.listbox_boards.pack()
-
-		for item in BOARDS_4PLEBS:
-			self.listbox_boards.insert(END, item)
 
 		self.button_close = Button(master, text="Close", command=master.quit)
 		'''Quits when clicked.'''
 		self.button_close.pack()
 
-	def greet(self):
-		print("Greetings!")
-
 
 if __name__ == '__main__':
 	root = Tk()
-	my_gui = MyFirstGUI(root)
+	my_gui = BowserMainGUI(root)
 	root.mainloop()
