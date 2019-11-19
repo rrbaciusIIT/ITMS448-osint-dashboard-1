9d1cfa512202ee1f48c2b5ad6aac9e1ba8b29e24
Henry Post <HenryFBP@gmail.com>
2019-11-18 20:54:48 -0600

    make dummy plot

--- bowser/bowserGUI.py
+++ bowser/bowserGUI.py
@@ -1,4 +1,11 @@
-from tkinter import Tk, Label, Button, Listbox, END, EXTENDED, Frame, SUNKEN
+from tkinter import *
+
+import matplotlib
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+matplotlib.use('TkAgg')
 
 from bowserScraper import BOARDS_4PLEBS
 
@@ -11,12 +18,42 @@ def nothing():
 	print("I do nothing!")
 
 
+class PlotClass:
+	def __init__(self, window):
+		self.window = window
+		self.box = Entry(window)
+		self.button = Button(window, text="Show plot", command=self.plot_new_window)
+		self.box.pack()
+		self.button.pack()
+
+	def plot_new_window(self):
+		x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+		v = np.array([16, 16.31925, 17.6394, 16.003, 17.2861, 17.3131, 19.1259, 18.9694, 22.0003, 22.81226])
+		p = np.array([16.23697, 17.31653, 17.22094, 17.68631, 17.73641, 18.6368,
+					  19.32125, 19.31756, 21.20247, 22.41444, 22.11718, 22.12453])
+
+		fig = Figure(figsize=(6, 6))
+		a = fig.add_subplot(111)
+		a.scatter(v, x, color='red')
+		a.plot(p, range(2 + max(x)), color='blue')
+		a.invert_yaxis()
+
+		a.set_title("Estimation Grid", fontsize=16)
+		a.set_ylabel("Y", fontsize=14)
+		a.set_xlabel("X", fontsize=14)
+
+		new_window = Toplevel(root)
+
+		canvas = FigureCanvasTkAgg(fig, master=new_window)
+		canvas.get_tk_widget().pack()
+		canvas.draw()
+
+
 class BowserOptionsPane:
-	def __init__(self, parent):
+	def __init__(self, master):
+		self.master = master
 
-		self.parent = parent
-
-		self.frame_options = Frame(self.parent, width=200, height=200, relief=SUNKEN)
+		self.frame_options = Frame(self.master, width=200, height=200, relief=SUNKEN)
 		self.frame_options.pack()
 
 		self.listbox_boards = Listbox(self.frame_options, selectmode=EXTENDED)
@@ -35,6 +72,13 @@ class BowserMainGUI:
 
 		self.label_hello_world = Label(master, text="This is our first GUI!")
 		self.label_hello_world.pack()
+
+		self.frame_graph_control = Frame(self.master, relief=SUNKEN)
+		'''Test graph controls.'''
+		self.frame_graph_control.pack()
+
+		# Add a plot to the plot frame.
+		self.frame_plot = PlotClass(self.frame_graph_control)
 
 		self.button_greet = Button(master, text="Greet", command=greet)
 		self.button_greet.pack()
