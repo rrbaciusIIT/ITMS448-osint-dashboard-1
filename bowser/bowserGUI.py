e0adea77984f837eb3a581e18e5f56447ac955aa
Henry Post <HenryFBP@gmail.com>
2019-11-18 20:56:03 -0600

    fix short var name

--- bowser/bowserGUI.py
+++ bowser/bowserGUI.py
@@ -33,14 +33,14 @@ class PlotClass:
 					  19.32125, 19.31756, 21.20247, 22.41444, 22.11718, 22.12453])
 
 		fig = Figure(figsize=(6, 6))
-		a = fig.add_subplot(111)
-		a.scatter(v, x, color='red')
-		a.plot(p, range(2 + max(x)), color='blue')
-		a.invert_yaxis()
+		axis = fig.add_subplot(111)
+		axis.scatter(v, x, color='red')
+		axis.plot(p, range(2 + max(x)), color='blue')
+		axis.invert_yaxis()
 
-		a.set_title("Estimation Grid", fontsize=16)
-		a.set_ylabel("Y", fontsize=14)
-		a.set_xlabel("X", fontsize=14)
+		axis.set_title("Estimation Grid", fontsize=16)
+		axis.set_ylabel("Y", fontsize=14)
+		axis.set_xlabel("X", fontsize=14)
 
 		new_window = Toplevel(root)
 
