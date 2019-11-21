20e2e16d9008a45e1b4e7479a165ddf45d3d8a34
Henry Post <HenryFBP@gmail.com>
2019-11-21 15:56:08 -0600

    build client in CI to ensure it works

--- ci-scripts/build-client.sh
+++ ci-scripts/build-client.sh
@@ -2,4 +2,13 @@
 
 pushd client
 
-exit 1
+npm install
+
+npm run-script build
+
+# Don't run in BG yet -- this may be useless.
+## run in bg
+#npm start &
+#
+## this is the server pid
+#NPM_SERVER_PID=$!
