17b18bed9606499923d6263e5404494feffb4014
Daniel Denekew <ddenekew@hawk.iit.edu>
2019-11-20 20:07:18 -0500

    Revert "break things apart, because I can do this shyt?!!! i got a secret weaponnext to be powwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww!!!!!!!!!!!!!!!!!!!"
    This reverts commit 21264a31879a90b4a0f6d1e815c11cef38fabeb5.

---
+++ scrapy/testScrapyProject/testScrapyProject/spiders/aarchivedmoe.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from pprint import pprint
+from typing import List
+
+import scrapy
+from scrapy import Selector
+
+
+class AArchivedMoeSpider(scrapy.Spider):
+	name = 'archived.moe'
+	allowed_domains = ['archived.moe']
+	start_urls = ['https://archived.moe/pol/']
+
+	POST_CONTROLS_SELECTOR = 'header > .post_data > .post_controls'
+
+	def parse(self, response):
+		print("Hi! I'm parsing a response!")
+
+		for item in response.css(self.POST_CONTROLS_SELECTOR):
+			item: Selector
+
+			# print("Here's the whole thing:")
+			# pprint(item.get())
+
+			post_matches: List[str] = item.re(r'href=".+?/thread/\d+?/"')
+
+			if len(post_matches) != 0:
+				pprint(post_matches)
+
+			# post_url = item.re('').get()
+			#
+			#
+			# print("Just the URL link:")
+			# pprint(post_url)
