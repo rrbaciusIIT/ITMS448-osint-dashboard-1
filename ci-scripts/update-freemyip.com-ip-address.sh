c26fad8a983cfbd6f930bd97b19a43545ad28e08
Henry Post <HenryFBP@gmail.com>
2019-11-25 19:44:33 -0600

    add script to update ip for freemyip

---
+++ ci-scripts/update-freemyip.com-ip-address.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ ! -v ${FREEMYIP_TOKEN} ]]; then
+    echo "You must set FREEMYIP_TOKEN environment variable!"
+    exit 1
+fi
+
+curl -v -x https://freemyip.com/update?token=${FREEMYIP_TOKEN}&domain=bowser-web-app.freemyip.com
