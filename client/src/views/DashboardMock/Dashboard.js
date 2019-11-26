1ffb5136d26a5a3f76f5ab3f752913261e1920bd
Jay <baileyjarron@gmail.com>
2019-11-26 15:32:28 -0600

    added column chart to  dashboard

--- client/src/views/DashboardMock/Dashboard.js
+++ client/src/views/DashboardMock/Dashboard.js
@@ -116,31 +116,6 @@ export default function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <Icon>info_outline</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Post that passed Triggers</p>
-              <h3 className={classes.cardTitle}>
-                {posts.noContentFlagCount ? (
-                  <>
-                    {posts.noContentFlagCount} <small>posts</small>
-                  </>
-                ) : (
-                  <div>- - -</div>
-                )}
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <DateRange />
-                Last 24 Hours
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
             <CardHeader color="info" stats icon>
               <CardIcon color="info">
                 <Icon>developer_board</Icon>
@@ -164,7 +139,31 @@ export default function Dashboard() {
             </CardFooter>
           </Card>
         </GridItem>
-
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Post that passed Triggers</p>
+              <h3 className={classes.cardTitle}>
+                {posts.noContentFlagCount ? (
+                  <>
+                    {posts.noContentFlagCount} <small>posts</small>
+                  </>
+                ) : (
+                  <div>- - -</div>
+                )}
+              </h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <DateRange />
+                Last 24 Hours
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
             <CardHeader color="danger" stats icon>
@@ -397,11 +396,47 @@ export default function Dashboard() {
             </CardFooter>
           </Card>
         </GridItem> */}
-        <GridItem xs={12} sm={12} md={4}>
+        <GridItem xs={12} sm={12} md={6}>
           <Card chart>
             <CardHeader>
               <MyChart
                 type="Pie"
+                labels={[
+                  `Terroism(${posts.terroismFlagCount})`,
+                  `NSA PRISM(${posts.nsaPrismFlagCount})`,
+                  `NSA ECHELON(${posts.nsaEchelonFlagCount})`,
+                  `Hate Speech(${posts.hateSpeechFlagCount})`,
+                  `Conspiracy(${posts.conspiracyFlagCount})`,
+                  `Racism(${posts.racismFlagCount})`,
+                  `Passed(${posts.noContentFlagCount})`
+                ]}
+                series={[
+                  posts.terroismFlagCount,
+                  posts.nsaPrismFlagCount,
+                  posts.nsaEchelonFlagCount,
+                  posts.hateSpeechFlagCount,
+                  posts.conspiracyFlagCount,
+                  posts.racismFlagCount,
+                  posts.noContentFlagCount
+                ]}
+              ></MyChart>
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Completed Tasks</h4>
+              <p className={classes.cardCategory}>Last Campaign Performance</p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+          <Card chart>
+            <CardHeader>
+              <MyChart
+                type="Column"
                 labels={[
                   `Terroism(${posts.terroismFlagCount})`,
                   `NSA PRISM(${posts.nsaPrismFlagCount})`,
