415e8a1edbd38174e11f850e96854dd18568c49c
Cooper Van Kampen <coopervk@gmail.com>
2019-11-27 00:36:36 -0600

    Beautify the Python a little bit

--- bowser/redditsample.py
+++ bowser/redditsample.py
@@ -4,7 +4,9 @@ import time
 
 # Get JSON of a given subreddit
 def grab_subreddit(name):
-    return bowserScraper.httpGET_json('https://www.reddit.com/r/' + name + '.json')
+    return bowserScraper.httpGET_json('https://www.reddit.com/r/' +
+                                      name +
+                                      '.json')
 
 # Get list of threads of given subreddit
 def grab_threadlist(SR):
@@ -12,9 +14,12 @@ def grab_threadlist(SR):
 
 # Get messages from given thread
 def grab_messages(thread):
-    json = bowserScraper.httpGET_json(str('https://reddit.com' + thread)[0:-1] + '.json')
+    json = bowserScraper.httpGET_json(str('https://reddit.com' + thread)[0:-1] +
+                                      '.json')
     messages = []
-    messages.append(json[0]['data']['children'][0]['data']['title'] + ':' + json[0]['data']['children'][0]['data']['selftext'])
+    messages.append(json[0]['data']['children'][0]['data']['title'] +
+                    ':' +
+                    json[0]['data']['children'][0]['data']['selftext'])
     [messages.append(comment['data']['body']) for comment in json[1]['data']['children']]
     return messages
 
